4e564a10374e8e2a2458067736fbdf2e75a392e8
kal-byte <kal@kal-byte.co.uk>
2022-03-22 09:58:40 +0000

    Add Misc cog with source command.

---
+++ cogs/misc.py
@@ -0,0 +1,17 @@
+from deps import Bot, Context
+from discord.ext import commands
+
+
+async def setup(bot: Bot):
+    await bot.add_cog(Misc(bot))
+
+
+class Misc(commands.Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @commands.command(aliases=['src'])
+    async def source(self, ctx: Context[Bot]):
+        """Provides a link with the source code of the bot."""
+        url = 'https://github.com/okaykallum/chat_bot'
+        await ctx.send(f'Here\'s a link with the source of the bot:\n<{url}>')
