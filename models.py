a8307a7d1c176669d04b41a5aee590910f104589
kal-byte <kal@kal-byte.co.uk>
2022-03-23 16:43:14 +0000

    Add Guild model

--- models.py
+++ models.py
@@ -4,6 +4,11 @@ from donphan import Table, Column, SQLType
 __all__ = ('Prefix',)
 
 
+class Guilds(Table):
+    guild: Column[SQLType.BigInt] = Column(primary_key=True, nullable=False)
+    owoify: Column[SQLType.Boolean] = Column(default=False)
+
+
 class Prefix(Table):
-    guild: Column[SQLType.BigInt] = Column(unique=True, nullable=False)
+    guild: Column[SQLType.BigInt] = Column(unique=True, nullable=False, references=Guilds.guild)
     prefix: Column[SQLType.Text] = Column(unique=True, nullable=False)
