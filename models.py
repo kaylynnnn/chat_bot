5937e1348d17ed55274940dac5e104d4b44bbbc8
kal-byte <kal@kal-byte.co.uk>
2022-03-23 16:47:08 +0000

    Fix naming convention

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from donphan import Table, Column, SQLType
 
 
-__all__ = ('Prefix',)
+__all__ = ('Prefix', 'Guild',)
 
 
-class Guilds(Table):
+class Guild(Table):
     guild: Column[SQLType.BigInt] = Column(primary_key=True, nullable=False)
     owoify: Column[SQLType.Boolean] = Column(default=False)
 
 
 class Prefix(Table):
-    guild: Column[SQLType.BigInt] = Column(unique=True, nullable=False, references=Guilds.guild)
+    guild: Column[SQLType.BigInt] = Column(unique=True, nullable=False, references=Guild.guild)
     prefix: Column[SQLType.Text] = Column(unique=True, nullable=False)
