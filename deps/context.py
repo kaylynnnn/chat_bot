736474ce09c810514ad03582df7491068913b237
kal-byte <kal@kal-byte.co.uk>
2022-03-23 16:54:04 +0000

    Check for owoification.

--- deps/context.py
+++ deps/context.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import asyncpg
+import discord
 
 from discord.ext import commands
+from models import Guild
+
+from utils.owoify import owoify_embed, owoify_text
 
 if TYPE_CHECKING:
     from deps import Bot
@@ -12,3 +17,24 @@ __all__ = ('Context',)
 
 class Context(commands.Context):
     bot: Bot
+
+    async def _check_owoify(self, guild: discord.Guild | None):
+        if not guild:
+            return False
+
+        async with self.bot.db as conn:
+            record: asyncpg.Record = await Guild.fetch_row(conn, guild=guild.id)
+
+        return record['owoify']
+
+    async def send(self, content: str | None = None, **kwargs):
+        if await self._check_owoify(self.guild):
+            if content:
+                content = owoify_text(content)
+
+            embed: discord.Embed | None = kwargs.pop('embed')
+            if embed:
+                embed = owoify_embed(embed)
+                kwargs['embed'] = embed
+
+        await super().send(content, **kwargs)
